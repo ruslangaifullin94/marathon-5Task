95ea4395285ce687e94b909ad2399ba6d8af4331
Ruslan Gaifullin <gayfullin.r.i@yandex.ru>
2023-05-13 16:42:00 +0300

    Initial Commit

---
+++ 5 task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5 task
+//
+//  Created by Руслан Гайфуллин on 13.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
